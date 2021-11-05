dc07ef3ffc6917972f3a810160f417c8a121f516
wotnak <wotnak@pm.me>
2021-11-05 22:21:50 +0100

    hide covid info block

--- src/components/ImportantInfoBlock.js
+++ src/components/ImportantInfoBlock.js
@@ -2,10 +2,10 @@ import React from "react"
 
 const ImportantInfoBlock = () => (
   <>
-  <aside className="block--important-info">
+  {/* <aside className="block--important-info">
     Prosimy Klientów o wchodzenie do biura pojedynczo i zachowanie bezpiecznej odległości 2m od mechanika.&nbsp;<br/>
     Po wyjściu każdego klienta dezynfekujemy klamki i powierzchnie.
-  </aside>
+  </aside> */}
   </>
 )
 
