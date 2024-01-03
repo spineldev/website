b127012515a51c4c6910fe4607d6320c4fce6340
wotnak <wotnak@pm.me>
2024-01-03 08:28:04 +0100

    update special hours info, remove info about fraudulent emails

--- src/components/ImportantInfoBlock.js
+++ src/components/ImportantInfoBlock.js
@@ -2,18 +2,7 @@ import React from "react"
 
 const ImportantInfoBlock = () => (
   <>
-    <div className="block--latest-post">W dniach 25.12.2023 - 01.01.2024 firma nieczynna (inwentaryzacja).</div>
-    <div className="block--important-info">
-      <strong>UWAGA!</strong> W dniu 21.12.2023 po całej Polsce zostały rozesłane przez hakerów
-      fałszywe, zainfekowane Trojanem maile w którym ktoś podszywając się pod
-      naszą firmę wysyła zapytanie. Włamano się do poczty firmy z Warszawy
-      i wysyłane są z niej wiadomości zawierające pobrane z naszej strony internetowej logo i dane adresowe.
-      Proszę nie otwierać załączników. Proszę sprawdzać rozszerzenia
-      załączników. Proszę sprawdzać nadawcę z podpisem w mailu. Z naszej firmy
-      wychodzą maile tylko z domeny spinel.pl. Ataki proszę zgłaszać na
-      stronie <a href="https://incydent.cert.pl" target="_blank" rel="noreferrer">incydent.cert.pl</a>. Jeśli otworzyłeś załącznik i poniosłeś jakieś
-      straty, proszę zgłaszać na policję.
-    </div>
+    <div className="block--latest-post">W dniu 22.01.2024 firma będzie nieczynna (za 06.01).</div>
   </>
 )
 
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -8,7 +8,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons"
 import { faClock } from "@fortawesome/free-regular-svg-icons"
 
-const specialHours = "25.12 - 01.01 nieczynne"
+const specialHours = "22.01 - nieczynne za 06.01"
 
 const TopBar = (props) => {
   return (
